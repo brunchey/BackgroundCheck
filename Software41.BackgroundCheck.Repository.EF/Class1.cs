50f83a65b7c643bccbd50cc79a992c46f55159b6
dmobrienllc <dmobrienllc@gmail.com>
2014-07-29 18:49:36 -0500

    Added Repository Interface class

---
+++ Software41.BackgroundCheck.Repository.EF/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Software41.BackgroundCheck.Repository.EF
+{
+    public class Class1
+    {
+    }
+}
---
+++ Software41.BackgroundCheck.Repository/IApplicantRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Software41.BackgroundCheck.Domain;
+
+namespace Software41.BackgroundCheck.Repository
+{
+    interface IApplicantRepository
+    {
+        public IEnumerable<Applicant> GetApplicants();
+
+        public void Save(Applicant applicant);
+    }
+}
