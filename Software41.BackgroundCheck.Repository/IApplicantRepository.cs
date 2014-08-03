927636efb7310f480fea3ba486f75d2b7028d754
brunchey <brunchey@yahoo.com>
2014-08-03 08:26:34 -0500

    Adding Applicants and Applicant pages.  Required use of Bootstrap, JQuery, and Autofac.  Chose autofac for DI because I needed to have an instance of the Applicant Repository.  Made a fake Applicant Repository in the Fakes folder.  Adding Tests next

--- Software41.BackgroundCheck.Repository/IApplicantRepository.cs
+++ Software41.BackgroundCheck.Repository/IApplicantRepository.cs
@@ -4,10 +4,10 @@ using Software41.BackgroundCheck.Domain;
 
 namespace Software41.BackgroundCheck.Repository
 {
-    interface IApplicantRepository
+    public interface IApplicantRepository
     {
-        public IEnumerable<Applicant> GetApplicants();
+        IEnumerable<Applicant> GetApplicants();
 
-        public void Save(Applicant applicant);
+        void Save(Applicant applicant);
     }
 }
---
+++ Software41.BackgroundCheck.Web/App_Start/ContainerConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using Software41.BackgroundCheck.Repository;
+using Software41.BackgroundCheck.Web.Controllers;
+using Software41.BackgroundCheck.Web.TestingFakes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Software41.BackgroundCheck.Web
+{
+    public class ContainerConfiguration
+    {
+        public static void Configure()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterType<FakeApplicantRepository>().As<IApplicantRepository>();
+
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+            
+
+
+        }
+
+    }
+}
